c6fe824650708f39119347c39b161b8457093d04
sakai memoru <sakai.memoru@gmail.com>
2022-09-20 07:25:12 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using Modules;
+
+class Program{
+  public static void Main(){
+    var name = "C# replit";
+    var P = new Module();
+    P.DoMain(name);
+  }
+}
---
+++ modules/Module.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Modules
+{
+  internal class Module {
+    public void DoMain(string str){
+      Console.WriteLine($"Hello, {str}!!!");  
+    }
+  }
+  
+}
+
